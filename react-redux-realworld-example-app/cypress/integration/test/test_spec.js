c597b5090664278ee1d3a7d23adb20da9bb59556
Sergio Ugalde <serch037@gmail.com>
2020-11-09 12:42:41 -0600

    Break tests

--- react-redux-realworld-example-app/cypress/integration/test/test_spec.js
+++ react-redux-realworld-example-app/cypress/integration/test/test_spec.js
@@ -2,7 +2,7 @@ describe('My test', () => {
     it('Should sign-up', () => {
         cy.visit('http://localhost:4100/');
         cy.contains('Sign up').click()
-        cy.url().should('include', '/register')
+        cy.url().should('include', '/sign-up')
 
     })
 })
