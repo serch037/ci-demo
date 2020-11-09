2ac1550f306043f85867c8ca98c989be194ec04a
Sergio Ugalde <serch037@gmail.com>
2020-11-09 11:37:31 -0600

    Initial commit

---
+++ react-redux-realworld-example-app/cypress/integration/test/test_spec.js
@@ -0,0 +1,8 @@
+describe('My test', () => {
+    it('Should sign-up', () => {
+        cy.visit('http://localhost:4100/');
+        cy.contains('Sign up').click()
+        cy.url().should('include', '/register')
+
+    })
+})
